f30de969cecc3ee236c9e8ce8f852d70a8760731
Niyas <niyas75tk@gmail.com>
2024-02-20 14:49:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <div className=" text-[red] text-[40px]">hai</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
