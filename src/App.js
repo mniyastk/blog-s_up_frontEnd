a6f3ce8fec0b1ad8488a3fca8742021f4b129f73
SanuRaj-K <sanusrk29@gmail.com>
2024-02-22 17:15:02 +0530

    feat: Added some author feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import Login from "./pages/Author/Login";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/user/Home";
 import Register from "./pages/Author/Register";
-import Dashboard from "./pages/Author/Dashboard";
+import AuthorLayout from "./pages/Author/AuthorLayout";
+import Blogs from "./pages/Author/Account/Blogs";
+import Dashboard from "./pages/Author/Account/Dashboard";
+import Followers from "./pages/Author/Account/Followers";
+import Reports from "./pages/Author/Account/Reports";
+import Messages from "./pages/Author/Account/Messages";
 
 function App() {
   return (
@@ -11,7 +16,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/author/dashboard" element={<Dashboard />} />
+        <Route path="/author" element={<AuthorLayout />}>
+          <Route   path="/author/blogs" element={<Blogs />} />
+          <Route index path="/author" element={<Dashboard />} />
+          <Route path="/author/followers" element={<Followers />} />
+          <Route path="/author/reports" element={<Reports />} />
+          <Route path="/author/messages" element={<Messages />} />
+        </Route>
       </Routes>
     </div>
   );
