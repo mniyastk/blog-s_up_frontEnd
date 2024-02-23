326c237268ccee76ac43ccd58438607f5aa39fc9
SanuRaj-K <sanusrk29@gmail.com>
2024-02-23 15:52:30 +0530

    feat: Added some author feature

---
+++ src/pages/Author/Account/NewBlog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NewBlog() {
+  return (
+    <div>
+      <div className=" flex justify-center items-center w-screen  ">
+        <div className="">Create New Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default NewBlog;
