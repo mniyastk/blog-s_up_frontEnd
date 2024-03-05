b62bf709b504111902811924bfd7bca05b8e201b
SanuRaj-K <sanusrk29@gmail.com>
2024-03-05 15:56:10 +0530

    feat:chages

--- src/pages/Author/AuthorLayout.jsx
+++ src/pages/Author/AuthorLayout.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 function AuthorLayout() {
   const [flag, setFlag] = useState(true);
+  const history = useNavigate();
 
   const handleHamburg = () => {
     setFlag(false);
   };
   const handleHamburgClose = () => {
     setFlag(true);
+  };
+  const handleLogout = () => {
+    const response = window.confirm("Are you Want to Logout...?");
+    if (response) {
+      history("/");
+    }
   };
   return (
     <>
@@ -20,7 +27,7 @@ function AuthorLayout() {
               className="p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500 focus:bg-teal-500 focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden"
             >
               <svg
-                className="w-5 h-5 fill-current"
+                className="w-5 h-5 mr-3  fill-current"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +45,7 @@ function AuthorLayout() {
               className="p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500   focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden"
             >
               <svg
-                className="w-5 h-5 fill-current"
+                className="w-5 h-5 mr-3  fill-current"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -86,7 +93,7 @@ function AuthorLayout() {
                   />
                   <button className="rounded-tr-md rounded-br-md px-2 py-3 hidden md:block">
                     <svg
-                      className="w-4 h-4 fill-current"
+                      className="w-4 h-4 mr-3  fill-current"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -100,9 +107,12 @@ function AuthorLayout() {
                   </button>
                 </div>
                 <div id="menu" className="flex flex-col space-y-2">
-                  <Link to={"/author"} className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out">
+                  <Link
+                    to={"/author"}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
+                  >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -116,7 +126,7 @@ function AuthorLayout() {
                     className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
                   >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -125,9 +135,12 @@ function AuthorLayout() {
                     </svg>
                     <span className="">Blogs</span>
                   </Link>
-                  <Link to={"/author/reports"} className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+                  <Link
+                    to={"/author/reports"}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                  >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -141,9 +154,12 @@ function AuthorLayout() {
                     </svg>
                     <span className="">Reports</span>
                   </Link>
-                  <Link to={ "/author/messages"} className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+                  <Link
+                    to={"/author/messages"}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                  >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -153,10 +169,13 @@ function AuthorLayout() {
                     </svg>
                     <span className="">Messages</span>
                   </Link>
-                  
-                  <Link to={ "/author/newblog"} className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+
+                  <Link
+                    to={"/author/newblog"}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                  >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -169,9 +188,12 @@ function AuthorLayout() {
                     </svg>
                     <span className="">New Blog</span>
                   </Link>
-                  <Link to={ "/author/followers"} className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+                  <Link
+                    to={"/author/followers"}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                  >
                     <svg
-                      className="w-6 h-6 fill-current inline-block"
+                      className="w-6 h-6 mr-3  fill-current inline-block"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -179,6 +201,19 @@ function AuthorLayout() {
                       <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
                     </svg>
                     <span className="">Followers</span>
+                  </Link>
+                  <Link
+                    onClick={handleLogout}
+                    className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                  >
+                    <svg
+                      className=" mr-3 w-6 h-6   fill-current inline-block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+                    </svg>
+                    <span className="">Logout</span>
                   </Link>
                 </div>
               </div>
