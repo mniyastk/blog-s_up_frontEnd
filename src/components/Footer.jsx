270ddd396a3ac80c13a67e68d2e6fc2b33fe322b
Bijeesh M <bijeeshh.m@gmail.com>
2024-03-09 17:30:35 +0530

    fix:fixed the error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <footer class="relative  bg-blue-gray-200 pt-8 pb-6">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap text-left lg:text-left">
+            <div class="w-full lg:w-6/12 px-4">
+              <h4 class="text-3xl fonat-semibold text-blueGray-700">
+                Let's keep in touch!
+              </h4>
+              <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
+                Find us on any of these platforms.
+              </h5>
+              <div class="mt-6 lg:mb-0 mb-6">
+                <button
+                  class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i class="fab fa-twitter"></i>
+                </button>
+                <button
+                  class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i class="fab fa-facebook-square"></i>
+                </button>
+                <button
+                  class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i class="fab fa-dribbble"></i>
+                </button>
+                <button
+                  class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i class="fab fa-github"></i>
+                </button>
+              </div>
+            </div>
+            <div class="w-full lg:w-6/12 px-4">
+              <div class="flex flex-wrap items-top mb-6">
+                <div class="w-full lg:w-4/12 px-4 ml-auto">
+                  <span class="block uppercase text-blue-gray-500 text-sm font-semibold mb-2">
+                    Useful Links
+                  </span>
+                  <ul class="list-unstyled text-blue-gray-500">
+                    <li>About Us</li>
+                    <li>Blog</li>
+                    <li>Github</li>
+                    <li>Free Products</li>
+                  </ul>
+                </div>
+                <div class="w-full lg:w-4/12 px-4">
+                  <span class="block uppercase text-blue-gray-500 text-sm font-semibold mb-2">
+                    Other Resources
+                  </span>
+                  <ul class="list-unstyled text-blue-gray-500">
+                    <li>MIT License</li>
+                    <li>Terms &amp; Conditions</li>
+                    <li>Privacy Policy</li>
+                    <li>Contact Us</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="my-6 border-blueGray-300" />
+          <div class="flex flex-wrap items-center md:justify-between justify-center">
+            <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div class="text-sm text-blueGray-500 font-semibold py-1">
+                Copyright ©{" "}
+                <span id="get-current-year">2021 Blog's Up Team</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Georgia: ['source-serif-pro', 'Georgia, Cambria', "Times New Roman", 'Times', 'serif'],
+        Sohnia:['sohne', "Helvetica Neue", 'Helvetica', 'Arial', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 });
